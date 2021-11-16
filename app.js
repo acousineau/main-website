c30971a0fa858e03020799b3be0080bdcaf4d603
Andy Cousineau <andrew.cousineau@gmail.com>
2021-11-16 15:10:54 -0500

    New message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hi there! This site is a work in progress!')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
