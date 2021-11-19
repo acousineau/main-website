b1916f8b885c45d1c40ec86a023516beeae179db
Andy Cousineau <andrew.cousineau@gmail.com>
2021-11-18 20:16:22 -0500

    Teapot status

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hi there! This site is a work in progress! Come back soon 😏')
 })
 
+app.get('/teapot', (req, res) => {
+  res.set('X-Teapot', 'Brew')
+  res.status(418)
+  res.send('I prefer coffee')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
